95c3b460b49711a374dc133f665fc21230b2270c
blujaee <audrey.e.bricker@gmail.com>
2023-04-22 00:16:58 -0400

    file update and README update but this time without leaking my database pw

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # BLU_RL
 
 This project is a simple URL shortener. 
-It is a work in progress, but when finished, it should take a user inputted URL and change it into a shorter URL using a hash algorithm.
+When finished, it should take any URL inputted by a user using a HTML form and change it into a shorter URL using a hash algorithm, then return the shorter URL to the user, storing all of the information in a MySQL database.
+---
+Date Started: 04/19/2023
 
+Date Finished:
+---
+## Tools Used
+- Python
+- Flask
+- MySQL
+- HTML, CSS, JavaScript
+---
 This will take you to my playlist for coding this project: [HERE](https://open.spotify.com/playlist/3L5YxqbmeOWjqIjR8sxobl?si=44077f5b03214dd1)
---
+++ first_file.py
@@ -0,0 +1,54 @@
+import mysql.connector
+
+import hashlib
+
+from flask import Flask, render_template, request
+
+from datetime import datetime
+
+import random
+
+import config
+
+blurl = mysql.connector.connect(
+    host=config.DB_HOST,
+    user=config.DB_USER,
+    password=config.DB_PASSWORD,
+    database=config.DB_DATABASE
+)
+
+mycursor = blurl.cursor()
+
+app = Flask(__name__)
+
+@app.route('/')
+def url_form():
+    return render_template('url_form.html')
+
+@app.route('/shorten', methods=['POST'])
+def shorten_url():
+    original_url = request.form['original_url']
+    hash_object = hashlib.sha256(original_url.encode())
+    hash_hex = hash_object.hexdigest()
+    random_addition = str(random.random()).encode('utf-8')
+    short_url = (request.host_url + hash_hex[:8] + random_addition.decode('utf-8')).encode('utf-8')
+    sql = "INSERT INTO urls (original_url, date_created, hash, short_url) VALUES (%s, %s, %s, %s)"
+    val = (original_url, datetime.now(), hash_hex, short_url)
+    mycursor.execute(sql, val)
+    blurl.commit()
+    
+    return "Long URL stored in database"
+
+@app.route('/<short_url>')
+def return_url(short_url):
+    sql = "SELECT original_url FROM urls WHERE short_url = %s"
+    val = (short_url,)
+    mycursor.execute(sql, val)
+    result = mycursor.fetchone()
+    if result is None:
+        return "URL not found"
+    else:
+        return result[0]
+    
+if __name__ == '__main__':
+    app.run(debug=True)
